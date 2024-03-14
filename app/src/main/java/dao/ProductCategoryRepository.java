198c86f64f1ff438506b97e610d0f76d9ecb3299
Daniel Kubong <kadmofor@gmail.com>
2024-03-14 00:11:53 -0400

    Added files to the dao package

---
+++ app/src/main/java/dao/ProductCategoryRepository.java
@@ -0,0 +1,12 @@
+package dao;
+
+import entities.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin
+@RepositoryRestResource(collectionResourceRel = "ProductCategory", path = "product-category")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+}
--- app/src/main/java/dao/ProductRepository.java
+++ app/src/main/java/dao/ProductRepository.java
@@ -1,11 +1,12 @@
 package dao;
 
-import entities.Address;
+import entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@Repository
+@RepositoryRestResource
 @CrossOrigin
-public interface AddressRepository extends JpaRepository<Address, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
