c6f344028df706e5c3ba0c40243c7fc7e8087210
Daniel Kubong <kadmofor@gmail.com>
2024-03-21 01:21:16 -0400

    Modified productRepo to search by categoryId

--- app/src/main/java/com/simple_city/app/dao/ProductRepository.java
+++ app/src/main/java/com/simple_city/app/dao/ProductRepository.java
@@ -1,11 +1,15 @@
 package com.simple_city.app.dao;
 
 import com.simple_city.app.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource
 @CrossOrigin(origins = "http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
