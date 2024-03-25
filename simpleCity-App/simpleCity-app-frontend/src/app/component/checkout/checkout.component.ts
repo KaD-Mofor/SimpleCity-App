865358fbb71a7b75edb7ccc3b625a84f5422afaa
Daniel Kubong <kadmofor@gmail.com>
2024-03-25 03:18:59 -0400

    Added form validation for Customer details

--- simpleCity-App/simpleCity-app-frontend/src/app/component/checkout/checkout.component.ts
+++ simpleCity-App/simpleCity-app-frontend/src/app/component/checkout/checkout.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
@@ -9,9 +9,9 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class CheckoutComponent implements OnInit{
 
   checkoutFormGroup!: FormGroup;
-  totalPrice: number = 0;
-  totalQty: number = 0;
-  shipping: number = this.totalPrice * 0.05;
+  // totalPrice: number = 0;
+  // totalQty: number = 0;
+  // shipping: number = this.totalPrice * 0.05;
 
   constructor(private formBuilder: FormBuilder){}
 
@@ -19,10 +19,11 @@ export class CheckoutComponent implements OnInit{
     
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
-        firstName: [''],
-        lastName: [''],
-        email: [''],
-        phone: ['']
+        firstName: new FormControl('', [Validators.required, Validators.minLength(2), this.noWhitespaceValidator]),
+        lastName: new FormControl('', [Validators.required, Validators.minLength(2), this.noWhitespaceValidator]),
+        email: new FormControl('', 
+                [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'), this.noWhitespaceValidator]),
+        phone: new FormControl('', [Validators.pattern('^(\([0-9]{3}\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}$')])
       }),
       shippingAddress: this.formBuilder.group({
         street: [''],
@@ -47,13 +48,25 @@ export class CheckoutComponent implements OnInit{
         expYear: ['']
       })
     });
+
+    
   }
 
   onSubmit(){
+
+    if (this.checkoutFormGroup.invalid){
+      this.checkoutFormGroup.markAllAsTouched();
+    }
+
     console.log("Submit button: ");
     console.log(this.checkoutFormGroup.get('customer')?.value);
     console.log(this.checkoutFormGroup.get('shippingAddress')?.value);
   }
+
+  get customerFirstName() {return this.checkoutFormGroup.get('customer.firstName')};
+  get customerLastName() {return this.checkoutFormGroup.get('customer.lastName')};
+  get customerEmail() {return this.checkoutFormGroup.get('customer.email')};
+  get customerPhone() {return this.checkoutFormGroup.get('customer.phone')};
 
   sameAsShoppingAddress(event: Event): void {
     const mouseEvent = event as MouseEvent;
@@ -63,5 +76,9 @@ export class CheckoutComponent implements OnInit{
       this.checkoutFormGroup.controls['billingAddress'].reset();
     }
   }
-
+  // Custom validator to check for whitespace
+  noWhitespaceValidator(control: AbstractControl): ValidationErrors | null {
+    const isWhitespace = (control.value || '').trim().length === 0;
+    return isWhitespace ? { 'whitespace': true } : null;
+  }
 }
