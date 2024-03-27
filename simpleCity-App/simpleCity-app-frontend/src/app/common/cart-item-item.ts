c609c4b22e0b27cfb01bb246caaf2d73a54534c1
Daniel Kubong <kadmofor@gmail.com>
2024-03-27 00:12:12 -0400

    Updated frontend classes and set up the checkout service REST API

--- simpleCity-App/simpleCity-app-frontend/src/app/common/cart-item-item.ts
+++ simpleCity-App/simpleCity-app-frontend/src/app/common/cart-item-item.ts
@@ -1,2 +1,21 @@
+import { CartItem } from "./cart-item";
+
 export class CartItemItem {
+
+    imageUrl: string;
+    quantity: number;
+    unitPrice: number;
+    productId: number; //change number to string
+
+    constructor(cartItem: CartItem) {
+         this.imageUrl = cartItem.imageUrl;
+         this.quantity = cartItem.quantity;
+         this.unitPrice = cartItem.unitPrice;
+         this.productId = cartItem.id;
+    }
+
+    // constructor(public imageUrl: string,
+    //     public unitPrice: number,
+    //     public quantity: number,
+    //     public productId: string) { }
 }
--- simpleCity-App/simpleCity-app-frontend/src/app/common/cart.ts
+++ simpleCity-App/simpleCity-app-frontend/src/app/common/cart.ts
@@ -1,2 +1,6 @@
 export class Cart {
+    constructor(
+        public totalQuantity: number,
+        public totalPrice: number,
+    ) { }
 }
