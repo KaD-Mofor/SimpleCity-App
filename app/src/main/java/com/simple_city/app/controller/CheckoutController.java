9dead61858b879c6b0700b3c6b9adc8c4f2b2875
Daniel Kubong <kadmofor@gmail.com>
2024-04-05 00:20:32 -0400

    Updated frontend to auto generate product category after reading from DB. Updated backend to make customer email unique and not duplicate customer.

---
+++ app/src/main/java/com/simple_city/app/controller/CheckoutController.java
@@ -0,0 +1,25 @@
+package com.simple_city.app.controller;
+
+
+import com.simple_city.app.dto.Purchase;
+import com.simple_city.app.dto.PurchaseResponse;
+import com.simple_city.app.service.CheckoutService;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:4200")
+@RestController
+@RequestMapping("/api/checkout")
+public class CheckoutController {
+
+    private final CheckoutService checkoutService;
+
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+
+        return checkoutService.placeOrder(purchase);
+    }
+}
---
+++ app/src/main/java/com/simple_city/app/dao/CustomerRepository.java
@@ -0,0 +1,13 @@
+package com.simple_city.app.dao;
+
+import com.simple_city.app.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@Repository
+@CrossOrigin
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Customer findByEmail(String email);
+   // Customer findByFirstNameAndLastName(String firstName, String lastName);
+}
