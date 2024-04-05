60721a5a5dcac3b19c964f99c6430fba22ec0ac6
Daniel Kubong <kadmofor@gmail.com>
2024-04-05 12:57:41 -0400

    Updated application to auto populate user email from session storage on checkout

--- simpleCity-App/simpleCity-app-frontend/src/app/common/cart-item-item.spec.ts
+++
@@ -1,7 +0,0 @@
-import { CartItemItem } from './cart-item-item';
-
-describe('CartItemItem', () => {
-  it('should create an instance', () => {
-    expect(new CartItemItem()).toBeTruthy();
-  });
-});
--- simpleCity-App/simpleCity-app-frontend/src/app/config/app-config.ts
+++
@@ -1,8 +0,0 @@
-export default {
-    oidc: {
-        clientId: '0oag4hoawrrsSNJoW5d7',
-        issuer: 'https://dev-52454893.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
-        scopes: ['openid', 'profile', 'email'],
-    }
-}
